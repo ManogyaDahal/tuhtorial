8dc3995af63f21c7557211ea03d15d375d16dc6c
Manogya Dahal <bat16692@gmail.com>
2026-09-04 13:10:45 +0545

    calculator using grpc and layered architecture

--- grpcCC/client/main.go
+++ grpcCC/client/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"time"
 
-	pb "ManogyaDahal/client/proto/gen/Add"
+	pb "ManogyaDahal/client/proto/gen/calculator"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -13,7 +13,7 @@ import (
 
 
 func main() {
-	addr := "localhost:8080"
+	addr := "localhost:5050"
 	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatal("Failed connection, err", err)
@@ -21,16 +21,16 @@ func main() {
 	defer conn.Close()
 
 	// defining client 
-	client := pb.NewAddNumberServiceClient(conn)
+	client := pb.NewCalculatorClient(conn)
 	ctx, cancel:= context.WithTimeout(context.Background(), time.Second * 10)
 	defer cancel()
 	
-	req := &pb.AddNumberRequest{ 
+	req := &pb.InputNumReq{ 
 		InputNum: []int64{2, 2, 2},
 	}
 	resp, err := client.AddNumber(ctx, req) //pulling Rpc method
 	if err != nil { 
 		log.Println("some error occured", err)
 	}
-	log.Println("obtained Sum is:", resp.Sum)
+	log.Println("obtained Sum is:", resp.Out)
 }
